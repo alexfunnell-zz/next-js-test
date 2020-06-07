20745d8d41af90e452492dd313f3235b78b8c760
Alex Funnell <vulcno@icloud.com>
2020-06-07 14:52:26 +0100

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,22 +2,23 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
-    return <Layout>
-        <Head>
-            <title>{postData.title}</title>
-        </Head>
-        <article>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-    </Layout>
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    )
 }
 
 export async function getStaticPaths() {
