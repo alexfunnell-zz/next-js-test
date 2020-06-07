4c4f9a867ce410d31238c8ce53a526cbdec3b48e
Alex Funnell <vulcno@icloud.com>
2020-06-07 18:29:13 +0100

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Alex Funnell'
+export const siteTitle = 'Next.js Test Blog'
 
 export default function Layout({
   children,
